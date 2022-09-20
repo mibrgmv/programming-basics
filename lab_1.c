3b1a180bef699f6ab8acfa9b296957fce16cedab
mibrgmv <mibrgmv@gmail.com>
2022-09-20 14:51:39 +0300

    lab_1 - done

---
+++ lab_1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int x; char y;
+    printf("Please, enter a decimal and a character: \n");
+    scanf("%d %c", &x, &y);
+    printf("%d %c", x, y);
+    return 0;
+};
+
+
+
