0ad7074c079d6ecd511697d59e993f453d359ec4
mibrgmv <mibrgmv@gmail.com>
2022-11-08 11:21:22 +0300

    Лаба 4

--- lab_4.c
+++ lab_4.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
-// -56 -- 56
+long inRange(int l, int r, int num) {
+    long a = (r - num) & 2147483648;
+    long b = (num - l) & 2147483648;
+    return a+b;
+}
 int main()
 {
     int x;
     scanf("%d", &x);
-    (-56 < x && x < 56) ? printf("True") : printf("False");
-
-
-
+    printf("%ld", !!inRange(-56, 56, x));
     return 0;
 }
 
+//    (-56 <= x && x <= 56) ? printf("True") : printf("False");
+//2**31 & x если тру то отрицательное
 
 
-
-
-
+//return (l <= num && num <= r);
