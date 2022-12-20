1c2e88f95f7be333a35d3b348502df5292931ac2
mibrgmv <mibrgmv@gmail.com>
2022-12-20 14:30:26 +0300

    Лаба 11

---
+++ lab11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lab11.h"
+
+//  Фигура - прямоугольный треугольник
+
+int main() {
+    struct RightTriangle T;
+    printf("Enter A coordinates: \n");
+    scanf("%lf %lf", &T.A.x, &T.A.y);
+    printf("Enter B coordinates: \n");
+    scanf("%lf %lf", &T.B.x, &T.B.y);
+    printf("Enter C coordinates: \n");
+    scanf("%lf %lf", &T.C.x, &T.C.y);
+    double P = Perimeter(&T);
+    double A = Area(&T);
+    printf("Perimeter - %lf; Area - %lf", P, A);
+}
